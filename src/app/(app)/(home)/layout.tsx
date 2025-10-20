87f4ded8a117c885ee2fe14fd2b5d5882d897e0f
xiangyun <3207904833@qq.com>
2025-10-20 18:57:19 +0800

    05-search-filters

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,16 +1,44 @@
-
+import configPromise from '@payload-config'
+import { getPayload } from 'payload'
 import { Footer } from "./Footer";
 import { Navbar } from "./Navbar";
+import { SearchFilters } from "./search-filters";
+import { Category } from '@/payload-types';
 
 interface Props {
     children: React.ReactNode;
 }
 
-const Layout = ({children} : Props) => {
+const Layout = async ({children} : Props) => {
+
+    const payload = await getPayload({
+        config: configPromise
+    })
+
+    const data = await payload.find({
+        collection: "categories",
+        pagination: false,
+        depth: 1,
+        where: {
+            parent: {
+                exists: false,
+            }
+        }
+    })
+
+    const formattedData = data.docs.map((doc) => ({
+        ...doc,
+        subcategories:(doc.subcategories?.docs ?? [].map((doc) => ({
+            ...(doc as Category),
+            subcategories: undefined
+        })))
+    }))
+    
     return (
     <>
         <div className="flex flex-col min-h-screen">
             <Navbar/>
+            <SearchFilters data={formattedData}/>
             <div className="flex-1 bg-[#f4f4f0]">{children}</div>
             <div><Footer /></div>
         </div>
