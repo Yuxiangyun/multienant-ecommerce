956326bf3b43689a2f08e9f1a09ba08b60571519
xiangyun <3207904833@qq.com>
2025-11-09 17:09:08 +0800

    07: trpc integration

--- src/app/(app)/(home)/search-filters/search-input.tsx
+++ src/app/(app)/(home)/search-filters/search-input.tsx
@@ -8,14 +8,14 @@ import { Button } from "@/components/ui/button";
 
 interface Props {
     disabled?:true;
-    data: CustomCategory[]
+
 }
 
-export const SearchInput = ({disabled, data}:Props) => {
+export const SearchInput = ({disabled}:Props) => {
     const [isSidebarOpen, setIsSiderbarOpen] = useState(false)
     return(
         <div className="flex items-center gap-2 w-full">
-            <CategoriesSidebar data={data} open={isSidebarOpen} onOpenChange={setIsSiderbarOpen}/>
+            <CategoriesSidebar  open={isSidebarOpen} onOpenChange={setIsSiderbarOpen}/>
             <div className="relative w-full">
                 <SearchIcon className="absolute left-3 top-1/2 -translate-y-1/2 size-4 text-neutral-500"/>
                 <Input className="pl-8" placeholder="Search products" disabled={disabled}/>
