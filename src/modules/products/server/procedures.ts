d409f03c7701f758b40f8c122e2cc51cec9f0abb
xiangyun <3207904833@qq.com>
2025-11-15 22:02:55 +0800

    12: filtering products

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -8,12 +8,25 @@ export const productsRouter = createTRPCRouter({
     getMany: baseProcedure
     .input(
         z.object({
-            category: z.string().nullable().optional()
+            category: z.string().nullable().optional(),
+            minPrice: z.string().nullable().optional(),
+            maxPrice: z.string().nullable().optional(),
         })
     )
 
     .query(async ({ctx, input}) => {
         const where: Where = {}
+        if(input.minPrice){
+            where.price = {
+                greater_than_equal: input.minPrice
+            }
+        }
+        if(input.maxPrice){
+            where.price = {
+                less_than_equal: input.maxPrice
+            }
+        }
+
         if(input.category){
             const categorysData = await ctx.db.find({
                 collection:"categories",
